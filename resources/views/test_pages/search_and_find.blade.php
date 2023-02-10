bb8a612c68a9d104d9be855ad8007c34a8bac91e
muhd-iskndr01-admin <muhd.iskndr01@gmail.com>
2023-02-10 11:01:37 +0800

    Added Laravel

---
+++ resources/views/test_pages/search_and_find.blade.php
@@ -0,0 +1,5 @@
+<body style="background-color:#FBFBFB;">
+<img src="/images/Main Logo.png" height=20% width=auto style="align:center; margin:auto; display:block; padding-top:250px;"></img><br>
+<input class="form-control" type="text">
+<img src="/icons/search.png" height=5% width=auto>
+</body>
