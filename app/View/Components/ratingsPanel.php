5b9567953259608f235482fbd6a93881c87d5f74
muhd-iskndr01-admin <muhd.iskndr01@gmail.com>
2023-04-22 23:42:21 +0800

    RatingsPanel

--- app/View/Components/ratingsPanel.php
+++ app/View/Components/ratingsPanel.php
@@ -6,7 +6,6 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 use App\Models\Rating;
-use App\Models\Library;
 use Illuminate\Support\Facades\Auth;
 
 class ratingsPanel extends Component
@@ -19,24 +18,15 @@ class ratingsPanel extends Component
     /**
      * Create a new component instance.
      */
-    public function __construct($id, $user=-1)
+    public function __construct($data)
     {
-        $data = Library::find($id);
         $this->library_id = $data->library_id;
         $this->library_name = $data->name;
 
-        // This means it will take the current user as the target
-        if($user == -1){
-            $user_id = Auth::user()->id;
-        } else {
-            $user_id = $user;
-        }
-
-        // Gets Update Data by the current user.
         $this->update_data = Rating::select('ratings.*', 'users.username')
         ->join('users', 'ratings.account_id', '=', 'users.id')
         ->where('library_id', $data->library_id)
-        ->where("account_id", $user_id)
+        ->where("account_id", Auth::user()->id)
         ->first();
     }
 
